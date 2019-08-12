65855f398b49a050ddc471a3829c83b8be7842c3
rondoni <dbieniasz2@yahoo.com>
2019-08-12 15:52:46 -0400

    s

--- src/main/java/com/project/test/UserTest.java
+++ src/main/java/com/project/test/UserTest.java
@@ -32,36 +32,43 @@ public class UserTest implements CommandLineRunner {
 	
 			
 			
-			/*
-			 * Machine m = new Machine(); m.setCode("AT-1300");
-			 * m.setDescription("something"); m.setHourly_rent(5.55);
-			 * m.setMax_hours_per_day(7.5);
-			 * 
-			 * Machine m2 = new Machine(); m2.setCode("NJ-90");
-			 * m2.setDescription("nothing"); m2.setHourly_rent(9.99);
-			 * m2.setMax_hours_per_day(2.0);
-			 * 
-			 * Timesheet timesheet = new Timesheet(); timesheet.setContractor_name("petey");
-			 * timesheet.setDate("9/22/19"); timesheet.setSite_code("gg-22");
-			 * 
-			 * 
-			 * machineService.add(m); machineService.add(m2);
-			 * 
-			 * MachineUse machineuse = new MachineUse(); machineuse.setHours_used(4.0);
-			 * machineuse.setMachine(m);
-			 * 
-			 * timesheet.addMachineUse(machineuse);
-			 * 
-			 * timesheetService.add(timesheet);
-			 * 
-			 * Timesheet t = timesheetService.getTimesheetById(timesheet.getId());
-			 * 
-			 * Set<MachineUse> mulist = t.getMachineuses();
-			 * 
-			 * System.out.println(t);
-			 * 
-			 * for (MachineUse mu : mulist) System.out.println(mu);
-			 */
+			
+			  Machine m = new Machine(); 
+			  m.setCode("AT-1300");
+			  m.setDescription("something"); 
+			  m.setHourly_rent(5.55);
+			  m.setMax_hours_per_day(7.5);
+			  
+			  Machine m2 = new Machine(); 
+			  m2.setCode("NJ-90");
+			  m2.setDescription("nothing"); 
+			  m2.setHourly_rent(9.99);
+			  m2.setMax_hours_per_day(2.0);
+			  
+			  Timesheet timesheet = new Timesheet(); 
+			  timesheet.setContractor_name("petey");
+			  timesheet.setDate("9/22/19"); 
+			  timesheet.setSite_code("gg-22");
+			  
+			  
+			  machineService.add(m); machineService.add(m2);
+			  
+			  MachineUse machineuse = new MachineUse(); 
+			  machineuse.setHours_used(4.0);
+			  machineuse.setMachine(m);
+			  
+			  timesheet.addMachineUse(machineuse);
+			  
+			  timesheetService.add(timesheet);
+			  
+			  Timesheet t = timesheetService.getTimesheetById(timesheet.getId());
+			  
+			  Set<MachineUse> mulist = t.getMachineuses();
+			  
+			  System.out.println(t);
+			  
+			  for (MachineUse mu : mulist) System.out.println(mu);
+			 
 
 		}
 	}
