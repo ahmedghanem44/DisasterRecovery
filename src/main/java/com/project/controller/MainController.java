9ac757e39f71a231f6ceb66707f508dabf965dff
Nemo <ahmedghanem44@yahoo.com>
2019-08-15 12:05:15 -0400

    r

--- src/main/java/com/project/controller/MainController.java
+++ src/main/java/com/project/controller/MainController.java
@@ -2,108 +2,174 @@ package com.project.controller;
 
 import java.util.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.project.model.Job;
-import com.project.service.JobHoursService;
-import com.project.service.JobService;
-import com.project.service.MachineService;
-import com.project.service.MachineUseService;
-import com.project.service.TimesheetService;
-import com.project.service.UserService;
+import com.project.beans.MachineBean;
+import com.project.model.*;
+import com.project.service.*;
 
 @Controller
 public class MainController {
-	
+
 	@Autowired
 	private JobService jobService;
 	@Autowired
 	private JobHoursService jobHours;
-	
+
 	@Autowired
 	private MachineService machineService;
 	@Autowired
 	private MachineUseService machineUse;
-	
+
 	@Autowired
 	private TimesheetService timeService;
-	
+
 	@Autowired
 	private UserService userService;
+
+	@Autowired
+	private MachineBean mb;
+
+	@GetMapping("/")
+	public ModelAndView root() {
+		ModelAndView mv = new ModelAndView("login");
+		return mv;
+	}
+
+	@RequestMapping(value = "/joblist", method = RequestMethod.GET)
+	public ModelAndView getJobList() {
+		Map<String, Object> mod = new HashMap<String, Object>();
+		mod.put("jobs", jobService.getAllJobs());
+		return new ModelAndView("JobManagementView", mod);
+	}
+
+	@RequestMapping(value = "/machinelist", method = RequestMethod.GET)
+	public ModelAndView getMachineList() {
+		Map<String, Object> mod = new HashMap<String, Object>();
+		mod.put("machines", machineService.getMachines());
+		return new ModelAndView("MachineManagementView", mod);
+	}
+
+	@RequestMapping(value = "/timesheetlist", method = RequestMethod.GET)
+	public ModelAndView getTimeSheetList() {
+		Map<String, Object> mod = new HashMap<String, Object>();
+		mod.put("timesheets", timeService.getTimesheets());
+		return new ModelAndView("timesheetAdd", mod);
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public ModelAndView login(HttpSession session, HttpServletRequest request, HttpServletResponse response) {
+
+		String uname = request.getParameter("username");
+		String pass = request.getParameter("password");
+		Map<String, Object> mod = new HashMap<String, Object>();
+
+		if (userService.isAuthorized(uname, pass)) {
+			mod.put("username", uname);
+			session.setAttribute("uname", uname);
+			if (userService.isAdminByUserName(uname))
+				return new ModelAndView("JobManagementView", mod);
+			else
+				return new ModelAndView("TimeSheetAdmin", mod);
+		} else
+			return new ModelAndView("error1");
+	}
+
+	@RequestMapping(value = "/deletemachine.html", method = RequestMethod.GET)
+	public ModelAndView deleteMachine(@RequestParam("id") Integer id) {
+		machineService.delete(id);
+		return new ModelAndView("redirect:/machinelist");
+	}
+
+	@RequestMapping(value = "/newmachine.html", method = RequestMethod.GET)
+	public ModelAndView newMachine(@ModelAttribute("command") Machine machine, BindingResult result) {
+
+		Map<String, Object> mod = new HashMap<String, Object>();
+		mod.put("machine", null);
+//		 mod.put("isUpdate", false);
+		ModelAndView mv = new ModelAndView("NewMachineView", mod);
+		return mv;
+	}
+
+
+	   @RequestMapping(value="/saveMachine" , method = RequestMethod.POST)
+	   public String addMachine(@ModelAttribute("command") Machine machine , BindingResult result) {
+  			System.out.println(machine.getId());
+
+	   		try {
+	   			Machine m = machineService.getMachineById(machine.getId()).get();
+	   			System.out.println(m.getId());
+	   			machineService.updateMachine(m);
+	   		}catch (NoSuchElementException e) {
+	   			System.out.println("Adding a new machine");
+	   			machineService.add(machine);
+	   		}	   			
+	   		return "redirect:/machinelist";
+	   }
+
+	@RequestMapping(value = "/editmachine.html", method = RequestMethod.GET)
+	public ModelAndView updateMachine(@ModelAttribute("command") Machine machine, BindingResult result) {
+		Machine machineToUpdate = machineService.getMachineById(machine.getId()).get();
+		Map<String, Object> mod = new HashMap<String, Object>();
+		mod.put("machine", machineToUpdate);
+//	   		mod.put("isUpdate", true);
+		return new ModelAndView("NewMachineView", mod);
+	}
+	
+	@RequestMapping(value = "/deletejob.html", method = RequestMethod.GET)
+	public ModelAndView deleteJob(@RequestParam("id") Integer id) {
+		jobService.removeJob(id);
+		return new ModelAndView("redirect:/joblist");
+	}
+	
+	@RequestMapping(value = "/newjob.html", method = RequestMethod.GET)
+	public ModelAndView newJob(@ModelAttribute("command") Job job, BindingResult result) {
+
+		Map<String, Object> mod = new HashMap<String, Object>();
+		mod.put("job", null);
+//		 mod.put("isUpdate", false);
+		ModelAndView mv = new ModelAndView("NewJobView", mod);
+		return mv;
+	}
+	
+	 @RequestMapping(value="/saveJob" , method = RequestMethod.POST)
+	   public String addJob(@ModelAttribute("command") Job job , BindingResult result) {
+			jobService.saveJob(job);	   			
+	   		return "redirect:/joblist";
+	   }
+	
+	
+	
 	
 
-    @GetMapping("/")
-    public ModelAndView root() {
-        ModelAndView mv = new ModelAndView("timesheetAdd");
-        return mv;
-    }
-    
-    @RequestMapping(value="/joblist",method= RequestMethod.GET)
-    public ModelAndView getJobList() {
-    	Map<String,Object> mod = new HashMap<String,Object>();
-    	mod.put("jobs", jobService.getAllJobs());
-    	return new ModelAndView("JobManagementView",mod);
-    }
-    
-    @RequestMapping(value="/machinelist", method = RequestMethod.GET)
-    public ModelAndView getMachineList() {
-    	Map<String,Object> mod = new HashMap<String,Object>();
-    	mod.put("machines" , machineService.getMachines());
-    	return new ModelAndView("MachineManagementView",mod);
-    }
-    
-    @RequestMapping(value="/timesheetlist" , method = RequestMethod.GET)
-    public ModelAndView getTimeSheetList() {
-    	Map<String,Object> mod = new HashMap<String,Object>();
-    	mod.put("timesheets", timeService.getTimesheets());
-    	return new ModelAndView("timesheetAdd" , mod);
-    }
-    
-    @RequestMapping(value="/login" , method = RequestMethod.POST)
-    public ModelAndView login(@RequestParam("username") String uname ,@RequestParam("password") 
-    String pass,HttpSession session) {
-    	Map<String,Object> mod = new HashMap<String,Object>();
-    	if (userService.isAuthorized(uname, pass)) {
-    		mod.put("username", uname);
-    		session.setAttribute("uname", uname);
-    		if (userService.isAdminByUserName(uname)) 
-    			return new ModelAndView("JobManagementView",mod);
-    		else 
-    			return new ModelAndView("TimeSheetView",mod);
-    	}else
-    		return new ModelAndView("error");
-    }
-    
-    @RequestMapping(value="/logout",method = RequestMethod.GET)
-    public String logout(HttpSession session) {
-    	session.removeAttribute("uname");
-    	session.invalidate();
-    	return "redirect:/login.html";
-    }
-    
-    @RequestMapping(value="/addJob" , method = RequestMethod.POST)
-    public String addJob(@ModelAttribute("command") Job job , BindingResult result) {
-    	jobService.saveJob(job);
-    	return "redirect:/joblist.html";
-    }
-    
-//    @RequestMapping(value="/deleteJob" , method = RequestMethod.POST)
-//    public String deleteJob(@ModelAttribute("command")  , BindingResult result) {
-//    	jobService.removeJob();
-//    	return "redirect:/joblist.html";
+//    @RequestMapping(value="/logout",method = RequestMethod.GET)
+//    public String logout(HttpSession session) {
+//    	session.removeAttribute("uname");
+//    	session.invalidate();
+//    	return "redirect:/login.html";
+//    }    
+
+//    
+//    @RequestMapping(value="/approve" , method = RequestMethod.POST)
+//    public String approveTimesheet(@ModelAttribute("command") Timesheet timesheet , BindingResult result) {
+//    	timeService.approveTimeSheet(timesheet.getId());
+//    	return "redirect:/timesheetlist.html";
 //    }
-    
-    
-    
+
 
 }
--- src/main/java/com/project/dao/MachineDAO.java
+++ src/main/java/com/project/dao/MachineDAO.java
@@ -1,9 +1,27 @@
 package com.project.dao;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.project.model.Machine;
 
-public interface MachineDAO extends CrudRepository<Machine, Long> {
+public interface MachineDAO extends CrudRepository<Machine, Integer> {
+	
+	
+	@Transactional
+	@Modifying
+	@Query("update Machine m set m.code= :code , m.description= :description, m.hourly_rent= :hourly_rent,"
+			+ "m.max_hours_per_day= :max_hours_per_day where m.id= :id ")
+	void editMachine(@Param("code") String code, @Param("description") String description,
+			@Param("hourly_rent") double hourly_rent, @Param("max_hours_per_day") double max_hours_per_day,
+			@Param("id") int id);
+
+
+
 
 }
